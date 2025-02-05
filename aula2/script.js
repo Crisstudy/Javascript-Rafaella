39437caf301c066cf6d3fa7ffe66bbb292c1972e
CRISTINA SARAIVA FIGUEIREDO <kristinasaraiva12@gmail.com>
2025-02-05 17:01:37 -0300

    aprendendo sobre condicionais

--- aula2/script.js
+++ aula2/script.js
@@ -1,25 +1,43 @@
 //adciona uma função na para executar
 function adicionarTarefa() {
-    //mensagem de sucesso na execução 
-    let mensagem = "Tarefa adicionada com sucesso!";
+   //armazena e imprime a tarefa
+       
         
         //recebe a tarefa a ser adcionada
-        let inputTarefa = document.getElementById("inputTarefa")
+        const inputTarefa = document.getElementById("inputTarefa")
         //é o valor da tarefa ou o nome da tarefa que foi adcionada
-        let tarefa = inputTarefa.value
-        //armazena e imprime a tarefa
-        console.log(tarefa)
-        //mostra a mensagem de "tarefa adicionada com sucesso"
-        document.getElementById("mensagem").textContent = mensagem;
+        let tarefa = inputTarefa.value.trim()
 
-        //cria uma lista a partir da tag ul
-        let listaTarefas = document.getElementById("listaTarefas")
-        //mostra a lista criada em ul através da tag li
-        let novaTarefa = document.createElement("li")
-        //acrescenta novas tarefas 
-        novaTarefa.textContent = tarefa
-        //coloca as novas tarefas dentro da lista de tarefas em ul
-        listaTarefas.appendChild(novaTarefa)
-        //limpa o campo do input para novos valores serem adicionados
-        inputTarefa.value = ""
+        const mensagem = document.getElementById("mensagem")
+
+        // se o valor do input for vazio então mostre uma mensagem de erro para o usuário
+            if (tarefa == "") {
+                 //mostre uma mensagem de erro
+                let mensagemErro = "Digite uma tarefa para adicionar a sua  lista."
+                mensagem.textContent = mensagemErro 
+                //mudar cor da mensagem para vermelho
+                mensagem.style.color = "red" 
+            } else {
+                //mensagem de sucesso na execução 
+                let mensagemSucesso = "Tarefa adicionada com sucesso!"; 
+                //mostra a mensagem de "tarefa adicionada com sucesso"
+                mensagem.textContent =   mensagemSucesso;  
+                //mudar cor da mensagem para verde
+                mensagem.style.color = "green" 
+                
+                //cria um novo item li
+                const listaTarefas = document.getElementById("listaTarefas")
+                // insere na lista ul
+                let novaTarefa = document.createElement("li")
+                //acrescenta novas tarefas 
+                novaTarefa.textContent = tarefa
+                //coloca as novas tarefas dentro da lista de tarefa em ul
+                listaTarefas.appendChild(novaTarefa)
+            
+            }
+
+                //limpa o campo do input para novos valores seremadicionados
+                inputTarefa.value = ""
+        
+
 }
