74a1a3c393ea6f12e4bbffa447f377f1e50f3465
CRISTINA SARAIVA FIGUEIREDO <kristinasaraiva12@gmail.com>
2025-02-10 18:59:20 -0300

    arrays

--- Aula3/aula-js-3-main/script.js
+++ Aula3/aula-js-3-main/script.js
@@ -14,6 +14,7 @@ function adicionarTarefa() {
         mensagem.textContent = mensagemErro
         //adiciona cor vermelha a mensagem 
         mensagem.style.color = "red"
+
     } else {
         //mensagem de tarefa adicionada com sucesso
         let mensagemSucesso = "Tarefa adicionada com sucesso!"
@@ -22,7 +23,7 @@ function adicionarTarefa() {
         mensagem.style.color = "green"
 
         tarefas.push(tarefa)
-        renderizarTarefas()
+        renderizarTarefas() 
     }
 
     //limpa o input do usuário
@@ -32,6 +33,8 @@ function adicionarTarefa() {
 function renderizarTarefas() {
     //cria novo item (li) e insere na (lista ul)
     const listaTarefas = document.getElementById("listaTarefas")
+    const botaoRemoverTudo = document.getElementById("botaoRemoverTudo")
+
     listaTarefas.innerHTML = ""
 
     //for itens na lista
@@ -40,10 +43,47 @@ function renderizarTarefas() {
     //3.. se vai de 1 em 1 elemento ou se pula
     
     //for (iterador, condição, frequencia)
-    let i = 0 
-    for (i; i < tarefas.length; i++) {
+    
+    for (let i = 0 ; i < tarefas.length; i++) {
         let novaTarefa = document.createElement("li")
         novaTarefa.textContent = tarefas[i]
+        
+        let botaoRemover = document.createElement("button")
+        botaoRemover.className = "remover"
+        botaoRemover.textContent = "Remover"
+        botaoRemover.onclick = () => removerTarefa(i)
+
+        let botaoEditar = document.createElement("button")
+        botaoEditar.className = "editar"
+        botaoEditar.textContent = "Editar"
+        botaoEditar.onclick = () => editarTarefa(i)
+        
+        novaTarefa.appendChild(botaoRemover)
+        novaTarefa.appendChild(botaoEditar)
         listaTarefas.appendChild(novaTarefa)
     }
+
+    // Mostrar o botão "Remover Tudo"apenas se houver tarefas na lista
+    botaoRemoverTudo.style.display = tarefas.length > 0 ? "block" : "none"
+}
+
+function removerTarefa(i) {
+    tarefas.splice(i, 1)
+    renderizarTarefas()
+}
+
+function editarTarefa(i) {
+    let tarefaEditada = prompt("Edite a tarefa:")
+    if (tarefaEditada.trim() !== "") {
+        tarefas[i] = tarefaEditada
+        renderizarTarefas()
+    }
+}
+
+function limparLista() {
+    tarefas.length = 0
+    renderizarTarefas()
+    const mensagem = document.getElementById("mensagem")
+    mensagem.textContent = "Lista de tarefas limpa com sucesso!"
+    mensagem.style.color = "white"
 }
